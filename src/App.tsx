2d84730297192cea0e308f0bfe78b17b6e06be59
yuann-se <103625509+yuann-se@users.noreply.github.com>
2022-08-09 23:54:37 +0300

    Header layout completed, HMR enabled.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './app.global.css';
+import { hot } from 'react-hot-loader/root'
+import { Header } from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello there</h1>
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
-export default App;
+export default hot(App);
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import styles from './header.module.css';
+import { Link } from "react-router-dom";
+import logo from '../../img/logo.jpg';
+
+export function Header() {
+  return (
+    <header>
+      <div className={styles.container}>
+        <Link to='/'>
+          <img src={logo} alt="" />
+        </Link>
+
+        <Link to='statistics' className={styles.statistics}>
+          <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M6 16H10V0H6V16ZM0 16H4V8H0V16ZM12 5V16H16V5H12Z" fill="#DC3E22" />
+          </svg>
+          <span>Статистика</span>
+        </Link>
+      </div>
+    </header>
+  );
+}
